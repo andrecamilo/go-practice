c723daf309470fd15f0e590bfc9ce139896ab7cd
andre.camilo <andrehcamilo@gmail.com>
2020-01-02 15:28:29 -0300

    alterado algoritmo

--- hacker/funnystring/main.go
+++ hacker/funnystring/main.go
@@ -5,24 +5,69 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"reflect"
 	"strconv"
 	"strings"
 )
 
 // Complete the funnyString function below.
 func funnyString(s string) string {
-	runes := []rune(s)
 
+	fmt.Println("iniciando funnyString")
+
+	runes := []rune(s)
 	var result []int
+	var diferencas []int
+	var reverseString string
+
+	for i := 0; i < len(runes); i++ {
+		reverseString = string(runes[i]) + reverseString
+	}
 
 	for i := 0; i < len(runes); i++ {
 		result = append(result, int(runes[i]))
 	}
 
+	resultReverse := make([]int, len(result))
+	for i, j := 0, len(result)-1; i <= j; i, j = i+1, j-1 {
+		resultReverse[i], resultReverse[j] = result[j], result[i]
+	}
+
+	for i := 0; i < len(result); i++ {
+		if (i + 1) < len(result) {
+			x := result[i] - result[i+1]
+			diferencas = append(diferencas, x)
+		}
+	}
+
+	diferencasReverse := make([]int, len(diferencas))
+	for i, j := 0, len(diferencas)-1; i <= j; i, j = i+1, j-1 {
+		diferencasReverse[i], diferencasReverse[j] = diferencas[j], diferencas[i]
+	}
+
+	fmt.Println("String: ")
+	fmt.Println(s)
+
+	fmt.Println("String reverso: ")
+	fmt.Println(reverseString)
+
+	fmt.Println("ASCII: ")
 	fmt.Println(result)
 
-	return s
+	fmt.Println("ASCII reverso: ")
+	fmt.Println(resultReverse)
 
+	fmt.Println("Diferencas ")
+	fmt.Println(diferencas)
+
+	fmt.Println("Diferencas reverso: ")
+	fmt.Println(diferencasReverse)
+
+	if reflect.DeepEqual(diferencasReverse, diferencas) {
+		return "Funny"
+	}
+
+	return "Not Funny"
 }
 
 func main() {
