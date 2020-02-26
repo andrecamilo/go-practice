f19d192fa0b6c1b4944f503baa4905c1141c9ad9
andre.camilo <andrehcamilo@gmail.com>
2020-02-26 14:31:56 -0300

    data sctructure

--- data_structure/main.go
+++ data_structure/main.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-	"../GoLang/bubblesort"
-	"../GoLang/exponenciacao"
-	"../GoLang/fatorial"
-	"../GoLang/fibonacci"
-	"../GoLang/hanoi"
-	"../GoLang/heapsort"
-	"../GoLang/caixeiroviajante"
-	"../GoLang/insertionsort"
-	"../GoLang/mergesort"
-	"../GoLang/selectionsort"
-	"../GoLang/shellsort"
-	"../GoLang/radixsort"
-	"../GoLang/quicksort"
-	"../GoLang/combsort"
-	"../GoLang/cocktailsort"
-	"../GoLang/countingsort"
-	"../GoLang/gnomesort"
 	"fmt"
+	"github.com/andrecamilo/go-practice/data_structure/bubblesort"
+	"github.com/andrecamilo/go-practice/data_structure/caixeiroviajante"
+	"github.com/andrecamilo/go-practice/data_structure/cocktailsort"
+	"github.com/andrecamilo/go-practice/data_structure/combsort"
+	"github.com/andrecamilo/go-practice/data_structure/countingsort"
+	"github.com/andrecamilo/go-practice/data_structure/exponenciacao"
+	"github.com/andrecamilo/go-practice/data_structure/fatorial"
+	"github.com/andrecamilo/go-practice/data_structure/fibonacci"
+	"github.com/andrecamilo/go-practice/data_structure/gnomesort"
+	"github.com/andrecamilo/go-practice/data_structure/hanoi"
+	"github.com/andrecamilo/go-practice/data_structure/heapsort"
+	"github.com/andrecamilo/go-practice/data_structure/insertionsort"
+	"github.com/andrecamilo/go-practice/data_structure/mergesort"
+	"github.com/andrecamilo/go-practice/data_structure/quicksort"
+	"github.com/andrecamilo/go-practice/data_structure/radixsort"
+	"github.com/andrecamilo/go-practice/data_structure/selectionsort"
+	"github.com/andrecamilo/go-practice/data_structure/shellsort"
 )
 
 func main() {
@@ -32,27 +32,27 @@ func main() {
 
 	slice := []int{5, 2, 1, 6, 9, 8, 7, 3, 4}
 	fmt.Println("Slice : ", slice)
-  bubblesort.BubbleSort(slice)
+	bubblesort.BubbleSort(slice)
 	fmt.Println("BubbleSort : ", slice, "\n")
 
 	slice = []int{1, 3, 2, 9, 7, 8, 5, 6, 4}
 	fmt.Println("Slice : ", slice)
-  selectionsort.SelectionSort(slice)
+	selectionsort.SelectionSort(slice)
 	fmt.Println("SelectionSort : ", slice, "\n")
 
 	slice = []int{9, 8, 7, 5, 1, 2, 4, 3, 6}
 	fmt.Println("Slice : ", slice)
-  insertionsort.InsertionSort(slice)
+	insertionsort.InsertionSort(slice)
 	fmt.Println("InsertionSort : ", slice, "\n")
 
 	slice = []int{4, 6, 7, 1, 2, 3, 5, 9, 8}
 	fmt.Println("Slice : ", slice)
-  mergesort.MergeSort(slice)
+	mergesort.MergeSort(slice)
 	fmt.Println("MergeSort : ", slice, "\n")
 
 	slice = []int{5, 1, 4, 2, 3, 6, 9, 7, 8}
 	fmt.Println("Slice : ", slice)
-  shellsort.ShellSort(slice)
+	shellsort.ShellSort(slice)
 	fmt.Println("ShellSort : ", slice, "\n")
 
 	slice = []int{6, 8, 7, 9, 1, 5, 4, 2, 3}
@@ -62,36 +62,36 @@ func main() {
 
 	slice = []int{6, 8, 7, 9, 1, 5, 4, 2, 3}
 	fmt.Println("Slice : ", slice)
-  heapsort.HeapSort2(slice)
+	heapsort.HeapSort2(slice)
 	fmt.Println("HeapSort2 : ", slice, "\n")
 
 	slice = []int{3, 1, 5, 4, 2, 9, 6, 8, 7}
 	fmt.Println("Slice : ", slice)
-  radixsort.RadixSort(slice)
+	radixsort.RadixSort(slice)
 	fmt.Println("RadixSort : ", slice, "\n")
 
 	slice = []int{2, 1, 8, 4, 3, 7, 6, 5, 9}
 	fmt.Println("Slice : ", slice)
-  quicksort.QuickSort(slice)
+	quicksort.QuickSort(slice)
 	fmt.Println("QuickSort : ", slice, "\n")
 
 	slice = []int{6, 5, 8, 7, 3, 4, 2, 1, 9}
 	fmt.Println("Slice : ", slice)
-  combsort.CombSort(slice)
+	combsort.CombSort(slice)
 	fmt.Println("CombSort : ", slice, "\n")
 
 	slice = []int{1, 5, 8, 3, 7, 4, 9, 6, 2}
 	fmt.Println("Slice : ", slice)
-  cocktailsort.CocktailSort(slice)
+	cocktailsort.CocktailSort(slice)
 	fmt.Println("CocktailSort : ", slice, "\n")
 
 	slice = []int{9, 5, 8, 7, 3, 2, 1, 6, 4}
 	fmt.Println("Slice : ", slice)
-  countingsort.CountingSort(slice)
+	countingsort.CountingSort(slice)
 	fmt.Println("CountingSort : ", slice, "\n")
 
 	slice = []int{2, 5, 1, 7, 4, 9, 8, 6, 3}
 	fmt.Println("Slice : ", slice)
-  gnomesort.GnomeSort(slice)
+	gnomesort.GnomeSort(slice)
 	fmt.Println("GnomeSort : ", slice, "\n")
 }
